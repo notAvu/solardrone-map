aff1f7e44b61181906f2703d85e6229ccb29d235
Álvaro Fernández <75076925+notAvu@users.noreply.github.com>
2023-10-11 13:30:00 +0200

    Insert spots

---
+++ main.js
@@ -0,0 +1,275 @@
+import './style.css';
+import GeoTIFF from 'ol/source/GeoTIFF.js';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import Overlay from 'ol/Overlay.js';
+import Projection from 'ol/proj/Projection.js';
+import MousePosition from 'ol/control/MousePosition.js';
+import { createStringXY } from 'ol/coordinate.js';
+import { defaults as defaultControls } from 'ol/control.js';
+import TileLayer from 'ol/layer/WebGLTile.js';
+import View from 'ol/View.js';
+import Point from 'ol/geom/Point.js';
+import Feature from 'ol/Feature.js';
+import { Icon, Style } from 'ol/style.js';
+import { Vector as VectorSource } from 'ol/source.js';
+import { Vector as VectorLayer } from 'ol/layer.js';
+import {Modify} from 'ol/interaction.js';
+class Spot{
+  constructor(id, coordinates ,label=""){
+      this.id= id;
+      this.coordinates = coordinates;
+      this.label = label;
+  }
+  getCoordinates() {
+      return this.coordinates;
+  }
+  getId(){
+      return this.id;
+  }
+  getSpotDict(){
+    var dict = {
+      'CoordX' : this.coordinates[0],
+      'CoordY' : this.coordinates[1],
+      'ID':this.id,
+        'label':this.label
+    };
+    return dict;
+}
+} 
+function getSpots(){
+  let spotsResult;
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://dotted-weaver-401511.ew.r.appspot.com/spots" );
+  xhttp.onreadystatechange = function() {
+    console.log('ApiCall')
+    if (this.readyState == 4 && this.status == 200) {
+      spotsResult = JSON.parse(this.responseText);
+      console.log(spotsResult);
+      loadSpots(spotsResult)
+    }
+    else{console.log(this.readyState+"||"+this.status)}
+  };
+  xhttp.setRequestHeader("Content-type", "application/json"); 
+  xhttp.send();
+  return spotsResult; 
+}
+/**
+  A function that converts coordinates from ESPG:3857 to ESPG:4326 
+  Parameters: coord -> int[2]: an array containing X and Y coordinates
+  */
+function epsg3857toEpsg4326(coord) {
+  const convertValue = 20037508.34;
+  let x = coord[0];
+  let y = coord[1];
+  x = (x * 180) / convertValue;
+  y = (y * 180) / convertValue;
+  y = (Math.atan(Math.pow(Math.E, y * (Math.PI / 180))) * 360) / Math.PI - 90;
+  return [x, y];
+}
+/**
+  A function that converts coordinates from ESPG:4326 to ESPG:3857 
+  Parameters: coord -> int[2]: an array containing X and Y coordinates
+  */
+function epsg4326toEpsg3857(coordinates) {
+  let x = coordinates[0];
+  let y = coordinates[1];
+  x = (coordinates[0] * 20037508.34) / 180;
+  y =
+    Math.log(Math.tan(((90 + coordinates[1]) * Math.PI) / 360)) /
+    (Math.PI / 180);
+  y = (y * 20037508.34) / 180;
+  return [x, y];
+}
+var spotList;
+function loadSpots(list){
+  spotList = list
+  console.log(spotList[0])
+  for (var s of spotList) {
+    console.log(s)
+    var coord = [s.CoordX, s.CoordY];
+    const newIcon = new Feature({
+      geometry: new Point(coord),
+      name: s.ID,
+    });
+    newIcon.setStyle(markerStyle);
+    vectorSource.addFeature(newIcon);
+  } 
+}
+
+const mousePositionControl = new MousePosition({
+  coordinateFormat: createStringXY(6),
+  // projection: 'EPSG:3857',
+  projection: 'EPSG:3857',
+});
+
+
+let vectorSource = new VectorSource();
+const baseMapLayer = new TileLayer({ source: new OSM() })
+const markerStyle = new Style({
+  image: new Icon({
+    anchor: [0.5, 1],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'fraction',
+    src: '/Images/Map_pin_icon.png',
+    scale: 0.02
+  }),
+});
+const vectorLayer = new VectorLayer({
+  source: vectorSource
+});
+
+var map = new Map({
+  controls: defaultControls().extend([mousePositionControl]),
+  target: 'map',
+  layers: [baseMapLayer, vectorLayer],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+
+const popupElement = document.getElementById('popup');
+const popup = new Overlay({
+  element: popupElement,
+  positioning: 'bottom-center',
+  stopEvent: false,
+});
+let popover;
+function disposePopover() {
+  if (popover) {
+    popover.dispose(); 
+    popover = undefined;
+  }
+}
+map.on('click', function (evt) {
+  const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+    return feature;
+  });
+  disposePopover();
+  if (feature) {
+    if(popupElement.style.display == 'none'){
+      popupElement.style.display = "block";
+    }
+    popup.setPosition(evt.coordinate);
+    // var fixedCoord = epsg3857toEpsg4326(evt.coordinate)
+    var fixedCoord = evt.coordinate
+    var coordText = document.getElementById('point-coordinates');
+    coordText.innerHTML = fixedCoord[0]+" X, "+fixedCoord[1]+" Y"
+    popover = new bootstrap.Popover(popupElement, {
+      placement: 'top',
+      html: true,
+    });
+    popover.show();
+  }
+});
+map.addOverlay(popup);
+map.on('movestart', disposePopover);
+
+function postSpotAPI(spot){
+  var body = JSON.stringify(spot);
+  console.log(body);
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://cors-anywhere.herokuapp.com/https://dotted-weaver-401511.ew.r.appspot.com/spots" );
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log('Spot inserted')
+    }
+    else{console.log("error: "+this.status)}
+  };
+  xhttp.setRequestHeader("Content-type", "application/json"); 
+  xhttp.send(body);
+}
+map.on('click', function (evt) {
+  let coordinate = evt.coordinate;
+  if (addButtonActive) {
+    let lastId;
+    let newIcon;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://dotted-weaver-401511.ew.r.appspot.com/spots/last" );
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        lastId = JSON.parse(this.responseText)[0]["MAX(ID)"];
+        
+        let spot = new Spot(lastId+1, coordinate);
+         newIcon = new Feature({
+          geometry: new Point(coordinate),
+          name: lastId+1,
+        });
+        newIcon.setStyle(markerStyle);
+    vectorSource.addFeature(newIcon);
+        postSpotAPI(spot.getSpotDict());
+      }
+    };
+    console.log(newIcon)
+  // xhttp.setRequestHeader("Content-type", "application/json"); 
+  xhttp.send();
+
+    
+    addButtonActive = false;
+  }
+});
+getSpots();
+const modify = new Modify({
+  hitDetection: vectorLayer,
+  source: vectorSource,
+});
+modify.on(['modifystart', 'modifyend'], function (evt) {
+  // evt.target.style.cursor = evt.type === 'modifystart' ? 'grabbing' : 'pointer';
+  console.log(evt.target);
+});
+// const overlaySource = modify.getOverlay().getSource();
+// overlaySource.on(['addfeature', 'removefeature'], function (evt) {
+  //   // evt.target.style.cursor = evt.type === 'addfeature' ? 'pointer' : '';
+  // });
+  map.addInteraction(modify);
+  
+  // const centerButton = document.getElementById('setCenter');
+  const closeButton = document.getElementById('close-button');
+  closeButton.addEventListener('click', closePopover);
+  const addPointButton = document.getElementById('addPoint');
+  const uploadButton = document.getElementById('uploadFile');
+  uploadButton.addEventListener('change', addGeotiffLayer);
+  addPointButton.addEventListener('click', addButtonClick);
+  // centerButton.addEventListener("click", setCenter);
+  function closePopover(){
+    
+    popupElement.style.display = 'none';
+    
+  }
+  var addButtonActive = false;
+  function setCenter() {
+    // const point = rgbSource.getTileGrid().getOrigin();  
+    // const size = map.getSize();
+    // map.getView().setZoom(17);
+    // map.getView().centerOn(point, size, [570, 500]);
+  }
+  
+  function addGeotiffLayer() {
+    var geotiffFile = uploadButton.files[0];
+    var fileUrl = URL.createObjectURL(geotiffFile);
+    var geotiffLayer;
+    fetch(fileUrl)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const geotiffSource = new GeoTIFF({
+        sources: [
+          {
+            blob: blob,
+          },
+        ],
+        convertToRGB: true,
+      });
+      geotiffLayer = new TileLayer({ source: geotiffSource, });
+      map.getLayers().insertAt(1, geotiffLayer);
+    });
+    // var center = geotiffLayer.getSources().getTileGrid().getOrigin();  
+    // var mapSize = map.getSize();
+    // map.getView().setZoom(17);
+    // map.getView().centerOn(center, mapSize, [570, 500]);
+  }
+  function addButtonClick() {
+    addButtonActive = true;
+  }
+  
